656efc752a73a52d73cb157fec300543cc935442
SergeyG <Cerega32@mail.ru>
2020-01-28 21:19:44 +0300

    Revert "Fixed"
    This reverts commit ad1b28560b20817dd72264a4fcc70e8749b08e28.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -34,7 +34,7 @@ export default class App extends Component {
         <RandomPlanet />
 
         <PeoplePage />
-        <Row left='foo' right='bar2'/>
+        <Row left='foo' right='bar'/>
       </div>
     );
   }
